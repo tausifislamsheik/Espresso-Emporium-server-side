c101ae3fd1fbf5cdedb425f8610d66cb27dcb6b3
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-24 03:00:45 +0600

    Delete functionality added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,11 +27,22 @@ async function run() {
 
     const coffeesCollection = client.db("coffeeDB").collection("coffees");
 
+    app.get('/coffees', async (req, res)=>{
+        const result = await coffeesCollection.find().toArray();
+        res.send(result)
+    })
+
     app.post('/coffees', async (req, res)=>{
         const newCoffee = req.body;
         const result = await coffeesCollection.insertOne(newCoffee);
-        res.send(result)
-        
+        res.send(result);
+    })
+
+    app.delete('/coffees/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeesCollection.deleteOne(query);
+        res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
