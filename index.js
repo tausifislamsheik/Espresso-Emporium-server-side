7c7d59c3c77420380c52d2ae432ef5d3657266aa
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-24 00:01:17 +0600

    coffee data provide to the database

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeesCollection = client.db("coffeeDB").collection("coffees");
+
+    app.post('/coffees', async (req, res)=>{
+        const newCoffee = req.body;
+        const result = await coffeesCollection.insertOne(newCoffee);
+        res.send(result)
+        
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
