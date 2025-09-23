a2b982dee43220492e5ad3cc31dfaba6c87c41e5
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-23 16:25:35 +0600

    Server added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('Coffee server is getting hotter!')
+})
+
+app.listen(port, ()=>{
+    console.log(`Coffee server is getting hotter on ${port}!`)
+})
