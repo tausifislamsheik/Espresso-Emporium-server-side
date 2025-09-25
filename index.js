a1745cd4413fd0b0d53224a61c023cf12b0a6b9c
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-25 17:11:52 +0600

    user profile data fix

--- index.js
+++ index.js
@@ -78,6 +78,25 @@ async function run() {
         res.send(result);
     })
 
+    app.patch('/users-profile', async (req, res)=>{
+        const {email, lastSignInTime} = req.body;
+        const filter = {email:email};
+        const updatedDoc = {
+          $set:{
+            lastSignInTime:lastSignInTime
+          }
+        }
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result)
+    })
+
+    app.delete('/users-profile/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
